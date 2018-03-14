8e6e15a5d6ab9a62c648fdbf1032356427033b97
shanellewebster <35905708+shanellewebster@users.noreply.github.com>
2018-03-13 22:36:02 -0500

    Add files via upload
    day 2 part 2

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first (arr){
+  return arr[0]
+};
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last (arr){
+  return arr[arr.length-1];
+}
 
 
 
@@ -47,7 +51,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper (family){
+  for (var i=0; i<arr.length; i++)
+    alert(family[i]);
+}
+looper(family);
 
 
 
@@ -62,9 +70,14 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-//Code Here
+function reversedLooper (a,b){
+  for (var i = letters.length - 1; i>=0; i--){
+    alert (letters[i])
+    console.log(letters[i])
+  }
+}
 
-
+reversedLoopers(letters);
 
 ////////// PROBLEM 5 //////////
 
@@ -77,7 +90,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return only the even numbers from the array.
 */
 
-//Code Here
+function evenFinder (nums){
+  for (var i = 0;i<nums.length; i++){
+    if (nums[i]%2 === 0){
+        return nums
+    }
+  }
+}
+eventFinder(nums);
 
 
 
