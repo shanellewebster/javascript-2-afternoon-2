e393bd4b13b20dbe2e44a26e81f46f08eaa232b2
shanellewebster <webstershanelle@gmail.com>
2018-03-19 18:07:25 -0500

    finished

--- practice.js
+++ practice.js
@@ -70,7 +70,7 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-function reversedLooper (a,b){
+function reversedLooper (letters){
   for (var i = letters.length - 1; i>=0; i--){
     alert (letters[i])
     console.log(letters[i])
@@ -91,13 +91,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 function evenFinder (nums){
+  var evens = []
   for (var i = 0;i<nums.length; i++){
     if (nums[i]%2 === 0){
-        return nums
-    }
+      evens.push(nums[i])
+    };
   }
+  return evens;
 }
-eventFinder(nums);
+evenFinder(nums);
 
 
 
